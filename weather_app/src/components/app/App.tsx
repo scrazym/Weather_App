63a92277c48704bb86f7f41685ed7c2cdc2aa770
scrazym <mil.grof@mail.ru>
2023-11-22 11:18:47 +0300

    refactor: add animation for bg

--- weather_app/src/components/app/App.tsx
+++ weather_app/src/components/app/App.tsx
@@ -3,7 +3,7 @@ import { useTheme } from "context/theme";
 import { Header } from "components/header/Header";
 import SideMenu from "components/sideMenu/SideMenu";
 
-import "./App.css";
+import "./App.scss";
 
 function App() {
   const { theme } = useTheme();
